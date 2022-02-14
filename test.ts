28a2e2fb52c59a7f957a82f6a6ae1304fa6bb3b3
Gang Zhang <gzhangx@hotmail.com>
2022-02-13 23:33:57 -0500

    test

--- test.ts
+++ test.ts
@@ -1,6 +1,17 @@
 import { gtMsDir } from './acccnMsGraphXls/src/lib/msdir';
 
+import creds from './acccnMsGraphXls/credentials.json'
 async function test() {
+    const dir = await gtMsDir({
+        tenantClientInfo: {
+            tenantId: creds.gzuser.tenantId,
+            client_id: creds.gzuser.client_id,
+        },
+        userId: creds.gzuser.userId,
+    });
+    dir.doGet(creds.dirInfo.NewGuestImageDir, `search(q='new')`).then(r => {
+        console.log(r);
+    })
 
 }
 
